4a5901bf2ea78ba898014edec559158720946f17
JQuinn <jack@masterbooker.com>
2019-12-29 12:52:49 +1100

    Css Module Architecture && Webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path =  require('path');
+const postCssPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-import'),
+    require('autoprefixer')
+];
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true,
+    module : {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCssPlugins }}]
+        }]
+    }
+}
